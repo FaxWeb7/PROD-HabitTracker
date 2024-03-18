a80734eb3de09d3fdc7cd0c6eeeba2dcda525abd
FaxWeb <80632493+FaxWeb7@users.noreply.github.com>
2024-03-19 00:34:51 +0300

    remove waste useEffect from App compoent

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
-import { FC, ReactNode, useEffect } from 'react'
+import { FC, ReactNode } from 'react'
 import { Header } from './components/layout/Header/Header'
-import { useDispatch, useSelector } from 'react-redux'
-import { currentDateActions, selectCurrentDate } from './store/currentDate/currentDate.slice'
 
 interface IAppProps {
   children: ReactNode
 }
 
 export const App: FC<IAppProps> = ({ children }) => {
-  const currentDateState = useSelector(selectCurrentDate)
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    if (currentDateState.value == ''){
-      dispatch(currentDateActions.setValue(new Date()))
-    }
-  }, [])
-  
   return (
     <div className='wrapper'>
       <Header />
