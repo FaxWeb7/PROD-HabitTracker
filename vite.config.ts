045f1ae8bc123e360303dc09426f5a7db8e9df83
FaxWeb <80632493+FaxWeb7@users.noreply.github.com>
2024-03-18 10:17:49 +0300

    set configs for progressive web application

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,36 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      outDir: 'dist',
+      manifest: {
+        name: 'Трекер привычек',
+        short_name: 'Привычки',
+        description: 'Приложение для отслеживания полезных привычек с геймификацией',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'logo/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo/android-chrome-256x256.png',
+            sizes: '256x256',
+            type: 'image/png'
+          },
+          {
+            src: 'logo/apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png'
+          },
+        ]
+      }
+    }),
+  ],
 })
---
+++ workbox-config.ts
@@ -0,0 +1,7 @@
+export default {
+  globDirectory: 'dist/',
+  globPatterns: [
+    '**/*/{scss,png,svg,jpg,jpeg,ts}'
+  ],
+  swDest: 'dist/sw.js',
+}
