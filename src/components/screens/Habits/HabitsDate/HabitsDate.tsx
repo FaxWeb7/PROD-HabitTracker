ef59903e6467ca8402cadee3a135b3edf42cfc9e
FaxWeb <80632493+FaxWeb7@users.noreply.github.com>
2024-03-19 23:44:20 +0300

    add HabitsDate component to work with dates on Habits screen

---
+++ src/components/screens/Habits/HabitsDate/HabitsDate.tsx
@@ -0,0 +1,26 @@
+import { ChangeEvent, FC, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { currentDateActions } from '../../../../store/currentDate/currentDate.slice';
+import { PrimaryButton } from '../../../shared/PrimaryButton/PrimaryButton';
+import styles from './habitsdate.module.scss'
+
+export const HabitsDate: FC = () => {
+  const [dateTime, setDateTime] = useState<Date>(new Date())
+  const dispatch = useDispatch()
+
+  const handleDateChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setDateTime(new Date(event.target.value))
+  }
+
+  const handleDateSubmit = () => {
+    dispatch(currentDateActions.setValue(dateTime))
+  }
+
+  return (
+    <div className={styles['habits-date']}>
+      <h2 className={styles['habits-date__text']}>Установка текущей даты</h2>
+      <input id='file-date' type='datetime-local' autoComplete='date' onChange={handleDateChange} className={styles['habits-date__input']} />
+      <PrimaryButton text='Готово' onClick={handleDateSubmit} className={styles['habits-date__button']} />
+    </div>
+  )
+}
