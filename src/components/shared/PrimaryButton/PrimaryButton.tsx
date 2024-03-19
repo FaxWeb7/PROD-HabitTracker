f11c74a7938e6bcd7cf4d5e10e0fa937ef9177aa
FaxWeb <80632493+FaxWeb7@users.noreply.github.com>
2024-03-19 13:37:29 +0300

    add className and type props to button component

--- src/components/shared/PrimaryButton/PrimaryButton.tsx
+++ src/components/shared/PrimaryButton/PrimaryButton.tsx
@@ -5,12 +5,14 @@ import styles from './primarybutton.module.scss';
 interface IPrimaryButton {
   icon: IconType
   text: string
-  onClick: MouseEventHandler<HTMLButtonElement>
+  onClick?: MouseEventHandler<HTMLButtonElement>
+  className?: string
+  type?: "button" | "submit" | "reset"
 }
 
 export const PrimaryButton: FC<IPrimaryButton> = (props: IPrimaryButton) => {
   return (
-    <button className={styles['button']} onClick={props.onClick}>
+    <button className={`${styles['button']} ${props?.className}`} type={props?.type} aria-autocomplete='none' onClick={props.onClick}>
       <props.icon className={styles['button__icon']} />
       <h3 className={styles['button__text']}>{props.text}</h3>
     </button>
