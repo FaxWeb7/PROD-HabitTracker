b2e0d9499d713a75de71a0197fa1052bf6847d4f
FaxWeb <80632493+FaxWeb7@users.noreply.github.com>
2024-03-19 23:37:51 +0300

    set icon props in shared button component not required

--- src/components/shared/PrimaryButton/PrimaryButton.tsx
+++ src/components/shared/PrimaryButton/PrimaryButton.tsx
@@ -3,8 +3,8 @@ import { IconType } from 'react-icons';
 import styles from './primarybutton.module.scss';
 
 interface IPrimaryButton {
-  icon: IconType
   text: string
+  icon?: IconType
   onClick?: MouseEventHandler<HTMLButtonElement>
   className?: string
   type?: "button" | "submit" | "reset"
@@ -13,7 +13,7 @@ interface IPrimaryButton {
 export const PrimaryButton: FC<IPrimaryButton> = (props: IPrimaryButton) => {
   return (
     <button className={`${styles['button']} ${props?.className}`} type={props?.type} aria-autocomplete='none' onClick={props.onClick}>
-      <props.icon className={styles['button__icon']} />
+      {props.icon && <props.icon className={styles['button__icon']} />}
       <h3 className={styles['button__text']}>{props.text}</h3>
     </button>
   )
