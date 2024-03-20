569b47a83e8b4a8204a1048792066e43b130a9e5
FaxWeb <80632493+FaxWeb7@users.noreply.github.com>
2024-03-20 23:49:05 +0300

    add useOutside hook for clicks outside of modal

---
+++ src/hooks/useOutisde.ts
@@ -0,0 +1,27 @@
+import { useEffect, useRef, useState } from "react"
+
+interface UseOutsideReturnType {
+  ref: React.LegacyRef<HTMLDivElement> | undefined
+  isShow: boolean
+  setIsShow: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+export const useOutside = (initialIsVisible: boolean): UseOutsideReturnType => {
+  const [isShow, setIsShow] = useState(initialIsVisible);
+  const ref = useRef<HTMLDivElement>(null);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (ref.current && !ref.current.contains(event.target as Node)) {
+      setIsShow(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside, true);
+    return () => {
+      document.removeEventListener('click', handleClickOutside, true);
+    };
+  }, []);
+
+  return { ref, isShow, setIsShow };
+};
