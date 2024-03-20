de3eb93b42d27a41cdb1cd711d363e63921292fe
FaxWeb <80632493+FaxWeb7@users.noreply.github.com>
2024-03-20 23:46:37 +0300

    add global interface to work with habit helpers

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,7 +1,15 @@
 import { IHabit } from "../models/UploadData/IHabit"
+import { IHabitAction } from "../models/UploadData/IHabitAction"
 
 export interface ISortedHabits {
   monthlyHabits: IHabit[]
   weeklyHabits: IHabit[]
   dailyHabits: IHabit[]
+}
+
+export interface IHabitHelper {
+  actions: IHabitAction[]
+  habit: IHabit
+  currentDate: string
+  period: number
 }
