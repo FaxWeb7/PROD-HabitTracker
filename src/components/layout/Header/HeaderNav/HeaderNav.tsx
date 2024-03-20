7af700e65b74a0e669e1897bb0268ff2f0d8ab0b
FaxWeb <80632493+FaxWeb7@users.noreply.github.com>
2024-03-20 23:59:37 +0300

    header nav change

--- src/components/layout/Header/HeaderNav/HeaderNav.tsx
+++ src/components/layout/Header/HeaderNav/HeaderNav.tsx
@@ -1,22 +1,23 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { HeaderNavList } from './HeaderNavList';
 import { CgMenu } from "react-icons/cg";
+import { useOutside } from '../../../../hooks/useOutisde';
+import { HeaderNavList } from './HeaderNavList';
 import styles from './headernav.module.scss';
 
 export const HeaderNav: FC = () => {
-  const [isMenuVisible, setIsMenuVisible] = useState<boolean>(false)
+  const { ref, isShow, setIsShow } = useOutside(false)
   const location = useLocation()
 
   return (
-    <nav className={styles['nav']}>
-      <button className={styles['nav__burger-button']} onClick={() => setIsMenuVisible(!isMenuVisible)}>
+    <nav className={styles['nav']} ref={ref}>
+      <button className={styles['nav__burger-button']} onClick={() => setIsShow(!isShow)}>
         <CgMenu className={styles['nav__burger-icon']} />
       </button>
-      <ul className={isMenuVisible ? `${styles['nav__list']} ${styles['active']}` : styles['nav__list']}>
+      <ul className={isShow ? `${styles['nav__list']} ${styles['active']}` : styles['nav__list']}>
         {HeaderNavList.map((navItem, index) => (
           <li key={index}>
-            <Link className={location.pathname == navItem.link ? `${styles['nav__item']} ${styles['active']}` : styles['nav__item']} to={navItem.link} onClick={() => setIsMenuVisible(false)}>
+            <Link className={location.pathname == navItem.link ? `${styles['nav__item']} ${styles['active']}` : styles['nav__item']} to={navItem.link} onClick={() => setIsShow(false)}>
               <navItem.icon className={styles['nav__item-icon']} />
               <h4 className={styles['nav__item-text']}>{navItem.title}</h4>
             </Link>
