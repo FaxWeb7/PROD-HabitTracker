b25a42c12fc5ae13f2ddeaecd23b59196b42c884
FaxWeb <80632493+FaxWeb7@users.noreply.github.com>
2024-03-21 17:42:53 +0300

    add helper that will find habit with max id

---
+++ src/helpers/Habits/GetMaxHabitId.ts
@@ -0,0 +1,9 @@
+import { IHabit } from "../../models/UploadData/IHabit";
+
+export const getMaxHabitId = (habits: IHabit[]) => {
+  let maxId = 0
+  habits.forEach((habit) => {
+    maxId = (habit.id > maxId ? habit.id : maxId)
+  })
+  return maxId
+}
