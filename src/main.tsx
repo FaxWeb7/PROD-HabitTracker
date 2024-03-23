0f9533f0cf14c031b1aa2edf14aee178e71aeb6d
FaxWeb <80632493+FaxWeb7@users.noreply.github.com>
2024-03-24 00:30:14 +0300

    remove profile route from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import { store } from './store/store.ts'
 import { HabitsScreen } from './components/screens/Habits/Habits.tsx'
 import { Dashboard } from './components/screens/Dashboard/Dashboard.tsx'
 import { Shop } from './components/screens/Shop/Shop.tsx'
-import { Profile } from './components/screens/Profile/Profile.tsx'
 import { Error } from './components/screens/Error/Error.tsx'
 import { App } from './App.tsx'
 import './styles/global.scss'
@@ -20,7 +19,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
             <Route path="/" element={<HabitsScreen />}/>
             <Route path="/dashboard" element={<Dashboard />}/>
             <Route path="/shop" element={<Shop />}/>
-            <Route path="/profile" element={<Profile />}/>
             <Route path="*" element={<Error />}/>
           </Routes>
         </App>
