268b106e576a0b5cf862462df3e2641b0a7be2e3
FaxWeb <80632493+FaxWeb7@users.noreply.github.com>
2024-03-23 18:23:51 +0300

    add constants that mean xp quantity for each of the period

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,2 +1,6 @@
 export const APP_URL: string = "http://192.168.1.75:5173"
-export const SITE_TITLE = "Трекер привычек"
+export const SITE_TITLE: string = "Трекер привычек"
+export const DAILY_EXPERIENCE: number = 150
+export const WEEKLY_EXPERIENCE: number = 1200
+export const MONTHLY_EXPERIENCE: number = 5000
+export const LEVEL_EXPERIENCE: number = 1500
